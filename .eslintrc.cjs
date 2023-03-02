85f9035e813d512fa582d7f58ad11ab2deb26e48
xxxsjan <626653354@qq.com>
2023-03-02 14:07:48 +0800

    eslint prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser' // ts默认有这个
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {}
+};
